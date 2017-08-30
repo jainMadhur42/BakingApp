50ea7284185b1bd924715bfe0c7ecdba5f63c318
WorldClock <jmadhur.kothadar42@gmail.com>
2017-08-30 01:35:06 -0700

    Testing Module Added

--- BackingApp/app/src/androidTest/java/anonestep/com/backingapp/RecipeDetailTest.java
+++ BackingApp/app/src/androidTest/java/anonestep/com/backingapp/RecipeDetailTest.java
@@ -1,10 +1,29 @@
 package anonestep.com.backingapp;
 
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
 
 /**
  * Created by welcome on 8/29/2017.
@@ -15,5 +34,44 @@ public class RecipeDetailTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+
+    @Test
+    public void recipeDetailTest() {
+        ViewInteraction recyclerView = onView(allOf(withId(R.id.recipe_list), isDisplayed()));
+        recyclerView.perform(actionOnItemAtPosition(1, click()));
+
+        onView(withText("Brownies")).check(matches(withText("Brownies")));
+
+        onView(withId(R.id.steps_recycler_view)).perform(scrollTo());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.short_description), withText("Recipe Introduction"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView.check(matches(withText("Recipe Introduction")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(org.hamcrest.Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 
 }
